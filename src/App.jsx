2cc78d9b655eaa0c75a5754f69dff7ddefb4202e
DeepakKadali <deepakkadali03@gmail.com>
2024-01-09 12:56:05 +0530

    weather-app

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [city, setCity] = useState("");
+  const [cityData, setCityData] = useState();
+  const [isApiLoading, setIsApiLoading] = useState(false);
+  const fetchData = async () => {
+    setIsApiLoading(true);
+    try {
+      const response = await axios.get(
+        `https://api.weatherapi.com/v1/current.json?q="${city}"&key=2a2bc82f88df41f089163800240901`
+      );
+      console.log(response);
+      setCityData(response.data);
+      setIsApiLoading(false);
+    } catch (error) {
+      alert("Failed to fetch weather data");
+      setIsApiLoading(false);
+      setCity("");
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="main">
+      <div className="input-container">
+        <input
+          className="input"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button className="button" onClick={() => fetchData()}>
+          Search
+        </button>
+      </div>
+      <div>{isApiLoading && <div>Loading Data...</div>}</div>
+      {!isApiLoading && cityData && (
+        <div className="feature-container">
+          <div className="feature">
+            <h2>Temparature</h2>
+            <div>
+              {cityData.current.temp_c}
+              <sup>o</sup>C
+            </div>
+          </div>
+          <div className="feature">
+            <h2>Humidity</h2>
+            <div>{cityData.current.humidity}%</div>
+          </div>
+          <div className="feature">
+            <h2>Condition</h2>
+            <div>{cityData.current.condition.text}</div>
+          </div>
+          <div className="feature">
+            <h2>Wind Speed</h2>
+            <div>{cityData.current.wind_kph} kph</div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
